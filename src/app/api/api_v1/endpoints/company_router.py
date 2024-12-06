a050c9f5b168ca4477e93ba228d19486d188d4d5
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-12-06 15:42:35 +0200

    [FEAT]: Add company router with endpoints for CRUD operations.

---
+++ src/app/api/api_v1/endpoints/company_router.py
@@ -0,0 +1,156 @@
+from uuid import UUID
+
+from fastapi import APIRouter, Depends, File, UploadFile, status
+from fastapi.responses import JSONResponse, StreamingResponse
+from sqlalchemy.orm import Session
+
+from app.schemas.common import FilterParams
+from app.schemas.company import CompanyCreate, CompanyUpdate
+from app.services import company_service
+from app.sql_app.database import get_db
+from app.utils.processors import process_request
+
+router = APIRouter()
+
+
+@router.get(
+    "/",
+    description="Retrieve all companies.",
+)
+def get_all_companies(
+    filter_params: FilterParams = Depends(), db: Session = Depends(get_db)
+) -> JSONResponse:
+    def _get_all_companies():
+        return company_service.get_all(filter_params=filter_params, db=db)
+
+    return process_request(
+        get_entities_fn=_get_all_companies,
+        status_code=status.HTTP_200_OK,
+        not_found_err_msg="No companies found",
+    )
+
+
+@router.get(
+    "/usernames/{username}",
+    description="Retrieve company by username.",
+)
+def get_company_by_username(
+    username: str,
+    db: Session = Depends(get_db),
+) -> JSONResponse:
+    def _get_company_by_username():
+        return company_service.get_by_username(username=username, db=db)
+
+    return process_request(
+        get_entities_fn=_get_company_by_username,
+        status_code=status.HTTP_200_OK,
+        not_found_err_msg=f"Company with username {username} not found",
+    )
+
+
+@router.get(
+    "/{id}",
+    description="Retrieve a company by its unique identifier.",
+)
+def get_company_by_id(id: UUID, db: Session = Depends(get_db)) -> JSONResponse:
+    def _get_company_by_id():
+        return company_service.get_by_id(id=id, db=db)
+
+    return process_request(
+        get_entities_fn=_get_company_by_id,
+        status_code=status.HTTP_200_OK,
+        not_found_err_msg=f"Company with id {id} not found",
+    )
+
+
+@router.post(
+    "/",
+    description="Create a new company.",
+)
+def create_company(
+    company_data: CompanyCreate,
+    db: Session = Depends(get_db),
+) -> JSONResponse:
+    def _create_company():
+        return company_service.create(company_data=company_data, db=db)
+
+    return process_request(
+        get_entities_fn=_create_company,
+        status_code=status.HTTP_201_CREATED,
+        not_found_err_msg="Company not created",
+    )
+
+
+@router.put(
+    "/{company_id}",
+    description="Update the current company.",
+)
+def update_company(
+    company_id: UUID,
+    company_data: CompanyUpdate,
+    db: Session = Depends(get_db),
+) -> JSONResponse:
+    def _update_company():
+        return company_service.update(
+            company_id=company_id,
+            company_data=company_data,
+            db=db,
+        )
+
+    return process_request(
+        get_entities_fn=_update_company,
+        status_code=status.HTTP_200_OK,
+        not_found_err_msg=f"Company with id {company_id} not updated",
+    )
+
+
+@router.post(
+    "/{company_id}/logo",
+    description="Upload a logo for the current company.",
+)
+def upload_logo(
+    company_id: UUID,
+    logo: UploadFile = File(),
+    db: Session = Depends(get_db),
+) -> JSONResponse:
+    def _upload_logo():
+        return company_service.upload_logo(
+            company_id=company_id,
+            logo=logo,
+            db=db,
+        )
+
+    return process_request(
+        get_entities_fn=_upload_logo,
+        status_code=status.HTTP_200_OK,
+        not_found_err_msg="Could not upload logo",
+    )
+
+
+@router.get(
+    "/{company_id}/logo",
+    description="Download the logo of the current company",
+)
+def download_logo(
+    company_id: UUID,
+    db: Session = Depends(get_db),
+) -> StreamingResponse:
+    return company_service.download_logo(company_id=company_id, db=db)
+
+
+@router.delete(
+    "/{company_id}/logo",
+    description="Delete the logo of the current company.",
+)
+def delete_logo(
+    company_id: UUID,
+    db: Session = Depends(get_db),
+) -> JSONResponse:
+    def _delete_logo():
+        return company_service.delete_logo(company_id=company_id, db=db)
+
+    return process_request(
+        get_entities_fn=_delete_logo,
+        status_code=status.HTTP_200_OK,
+        not_found_err_msg="Could not delete logo",
+    )
