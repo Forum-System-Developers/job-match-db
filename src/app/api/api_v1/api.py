fa02136a7a34519b7505070e3c331bdfa0779d1b
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-12-06 13:03:10 +0200

    [FEAT]: Include category router in API with prefix and tags.

--- src/app/api/api_v1/api.py
+++ src/app/api/api_v1/api.py
@@ -1,4 +1,10 @@
 from fastapi import APIRouter
 
+from app.api.api_v1.endpoints import category_router
 
 api_router = APIRouter()
+
+
+api_router.include_router(
+    category_router.router, prefix="/categories", tags=["Categories"]
+)
