500139efb0c44a3628e2b4f57dc866642782c9f5
Hristiyan <175059014+hbobev@users.noreply.github.com>
2024-12-06 13:24:25 +0200

    [FEAT]: Implement city router with endpoints to retrieve cities by various criteria.

--- src/app/api/api_v1/api.py
+++ src/app/api/api_v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import category_router
+from app.api.api_v1.endpoints import category_router, city_router
 
 api_router = APIRouter()
 
@@ -8,3 +8,5 @@ api_router = APIRouter()
 api_router.include_router(
     category_router.router, prefix="/categories", tags=["Categories"]
 )
+
+api_router.include_router(city_router.router, prefix="/cities", tags=["Cities"])
